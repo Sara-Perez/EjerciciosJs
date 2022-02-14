4705ac9a9d2c89c42b60ab9b5f47708b18cd40f2
sara <saraperezval5@gmail.com>
2022-02-14 19:55:21 +0100

    actualizacion

--- EJERCICIOS.js
+++ EJERCICIOS.js
@@ -140,41 +140,41 @@ function capitaliza(letter) {
 }
 
 // probamos la funcion
-console.log(capitaliza('barcelona lsU'));
+console.log(capitaliza('barcelona PARIS'));
 
 
 // DECIMO EJERCICIO //
 
 
-function palabra(letra = 'Hola') {
-    
-    let letrasTotales= 0;
-    let numVocales=0;
-    let numConsonantes=0;
-    let isPar;
-   
-    for ( i = 0; i < letra.length; i++) {
+function palabra(letra = '') {
 
-      
-        
-        if ( letra.charAt(i).toLocaleLowerCase() === 'a' 
-        || letra.charAt(i).toLocaleLowerCase() === 'e' 
-        || letra.charAt(i).toLocaleLowerCase() === 'i' 
-        || letra.charAt(i).toLocaleLowerCase() === 'o'
-        || letra.charAt(i).toLocaleLowerCase() ==='u') {
+    let letrasTotales = 0;
+    let numVocales = 0;
+    let numConsonantes = 0;
+    let isPar;
+
+    for (i = 0; i < letra.length; i++) {
+
+
+
+        if (letra.charAt(i).toLocaleLowerCase() === 'a'
+            || letra.charAt(i).toLocaleLowerCase() === 'e'
+            || letra.charAt(i).toLocaleLowerCase() === 'i'
+            || letra.charAt(i).toLocaleLowerCase() === 'o'
+            || letra.charAt(i).toLocaleLowerCase() === 'u') {
             numVocales++;
-            
-        }else{
+
+        } else {
             numConsonantes++;
         }
         letrasTotales++;
     }
-    isPar =  letrasTotales % 2 === 0;
+    isPar = letrasTotales % 2 === 0;
 
-    console.log(`"${letra}" tiene ${letra.length} letras.` );
-    console.log(`Vocales ${numVocales}` );
-    console.log(`Consonantes ${numConsonantes}` );
-    if(isPar) console.log('Es un número par');
+    console.log(`"${letra}" tiene ${letra.length} letras.`);
+    console.log(`Vocales ${numVocales}`);
+    console.log(`Consonantes ${numConsonantes}`);
+    if (isPar) console.log('Es un número par');
     else console.log('Es un número impar');
 }
 // probamos la funcion //
@@ -187,65 +187,102 @@ palabra('Barcelona');
 function hoy() {
     let numberOfTheDay = new Date().getDay();
     let days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-    let hoy = days[numberOfTheDay-1];
-console.log(`Hoy es ${hoy}`);
+    let hoy = days[numberOfTheDay - 1];
+    console.log(`Hoy es ${hoy}`);
 }
 
 hoy();
 
+
 // DOCEAVO EJERCICIO //
 
 function navidad() {
-    let numberOfTheDay = new Date().getDate();
-    let dias;
+    $days = console.getElementById("days"),
+        $hours = console.getElementById("hours"),
+        $minutes = console.getElementById("minutes"),
+        $seconds = console.getElementById("seconds"),
+        $finalMessage = console.getElementById(".final-sms");
 
-console.log(`Faltan ${dias} para navidad`);
-console.log(numberOfTheDay);
+    const countdownDate = new Date(" DEC 25, 2022 00:00:00").getTime();
+    let interval = setInterval(function () {
+        const now = new Date().getTime();
+
+        let distance = countdownDate - now;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000));
+
+        $days.console = days;
+        $hours.console = hours;
+        $minutes.console = minutes;
+        $seconds.console = ("0" + seconds).slice(-2);
+
+        if (distance < 0) {
+            clearInterval(interval);
+            $finalMessage.style.transform = "translateY(0)";
+        }
+
+    }, 1000);
 
 
 }
 
-navidad();
+
+
 
 // TRECEAVO EJERCICIO //
 
 function analiza(array) {
-
-  
-
-
-
+    let sum = 0;
+    let mayor = array[0];
+    let menor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        if (let(mayor < array[i])) {
+             (" el numero mayor es " + mayor);
+        }
+        if (let(menor > array[0])) {
+             (" el numero menor es " + menor);
+        }
+       
+    }
+    return("la suma de los" + "es igual a" + sum + "El numero mayor es" + mayor + "El numero menor es" + menor);
 }
 
-function prompExercise(){
-    
-    let number = Math.round(Math.random()*100);
-    console.log('number ',number);
-    let response = Number(prompt("Dime un número"));
-    let numeroDeVeces =0;
 
-    while(response != number){
+// Ejercicio final//
+
+
+function prompExercise() {
+
+    let number = Math.round(Math.random() * 100);
+    console.log('number ', number);
+    let response = Number(prompt("Dime un número"));
+    let numeroDeVeces = 0;
+
+    while (response != number) {
         response = Number(prompt("Dime un número"));
-        console.log('response ',response);
-        if(response === number) {
+        console.log('response ', response);
+        if (response === number) {
             console.log('Has acertado');
             numeroDeVeces++;
             console.log(`Has necesitado ${numeroDeVeces} veces`);
             break;
         }
-        else if(response > number) {
+        else if (response > number) {
             console.log('Demasiado alto');
-             numeroDeVeces++;
+            numeroDeVeces++;
         }
-        else if(response < number) {
+        else if (response < number) {
             console.log('Demasiado bajo');
-             numeroDeVeces++;
+            numeroDeVeces++;
         }
-        else  console.log('Caracter incorrecto');
+        else console.log('Caracter incorrecto');
     }
 
-    
+
 
 }
-prompExercise();
+//prompExercise();
 
